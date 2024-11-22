8f4e65b6053fccf31583c6f3b78f73c673b27d95
Fiqri Ardyansyah <fiqri191002@gmail.com>
2024-11-22 04:08:48 +0000

    vendor: Clone the private signing keys if possible
    Allow to clone private signing keys generated by developers.
    This makes signing keys more consistent because developers
    don't have to worry about signing keys being lost, or
    accidentally deleted.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -27,6 +27,7 @@ ifeq ($(IS_OFFICIAL),true)
 endif
 
 # Signing
+ifneq (,$(wildcard vendor/aosp/signing/keys))
 ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/aosp/signing/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/aosp/signing/keys/releasekey
@@ -34,5 +35,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
 ifneq (,$(wildcard vendor/aosp/signing/keys/otakey.x509.pem))
 PRODUCT_OTA_PUBLIC_KEYS := vendor/aosp/signing/keys/otakey.x509.pem
+endif
 endif
 endif
